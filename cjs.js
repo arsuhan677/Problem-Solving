e9ae7fdc9492c962cf834fd01cb0fa8893e84a95
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-05 00:32:33 +0600

    update for cjs

---
+++ cjs.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  add,
+  sub
+};
+
+// index.js
+
+const math = require("./math");
+
+console.log(math.add(10, 5)); // 15
+console.log(math.sub(10, 5)); // 5
