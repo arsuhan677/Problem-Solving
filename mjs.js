2aff9ee99471c2f7328fd8b0ab0a05e388a5b9e2
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-05 00:31:05 +0600

    update for mjs

---
+++ mjs.js
@@ -0,0 +1,14 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// index.js
+
+import { add, sub } from "./math.mjs";
+
+console.log(add(20, 10)); // 30
+console.log(sub(20, 10)); // 10
--- samevalue.js
+++ samevalue.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-  const students = ['abul'];
-=======
+
   const students = ['abul', 'babul', 'cabul', 'suhan', 'suhan', 'abul', 'babul'];
->>>>>>> 836bf983a2beee3f89000bc1a3a518c0108597f3
   const numbers = [1, 5, 61, 5, 87, 7, 5, 81, 61];
 
   function noDuplicate(array) {
