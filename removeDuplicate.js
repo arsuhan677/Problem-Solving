4725289d25cfa27d2cd9647a3179267495c0ebf1
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-16 02:34:12 +0600

    update for removeDuplicate

---
+++ removeDuplicate.js
@@ -0,0 +1,13 @@
+function removeDuplicate(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(
+  removeDuplicate([1, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 7, 8, 9, 9, 10, 11, 12])
+);
